03e50506380a882b70011ced645b5590193234c8
dknight <dknight@code.wm.edu>
2025-08-04 14:48:17 -0400

    reverse exercise

---
+++ reverse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace reverse;
+
+class Program
+{
+    /*
+     * ask user to input message
+     * print in order
+     * print in reverse
+     */
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a string: ");
+        string? input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input))
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i]);
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.WriteLine();
+
+            for (int j = input.Length - 1; j >= 0; j--)
+            {
+                Console.Write(input[j]);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+}
