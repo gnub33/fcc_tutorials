7087c658953eb3cbaef5cdc26c6adac7ee18f2aa
dknight <dknight@code.wm.edu>
2025-08-04 01:53:43 -0400

    add times table exercise

---
+++ Times_Table/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Times_Table;
+
+class Program
+{
+    /*
+     * Ask user for a number for the table
+     * Write a for loop to print x times table
+     * Add input validation with try/except
+     */
+    static void Main(string[] args)
+    {
+        int number = 0; // must be initialized
+        bool success = false;
+        while (!success)
+        {
+            Console.Write("Enter a number: ");
+            string? inputNum = Console.ReadLine(); 
+
+            if (int.TryParse(inputNum, out int num))
+            {
+                number = num;
+                success = true; // break loop
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+        
+        // once valid input is obtained
+        Console.WriteLine("Here is the times table: ");
+        for (int i = 1; i <= 12; i++)
+        {
+            Console.WriteLine("{0} x {1} = {2}", i, number, i * number);
+        }
+        
+    }
+}
